4cc3e7d3eadad0344140b17fc7aced872988a478
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2022-09-20 22:27:25 +0300

    ReCap Demo

---
+++ kodlamaio_homework/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       int number1 = 20, number2 = 25, number3 = 2 , largestNumber = number1;
+
+       if (largestNumber < number2){
+           largestNumber = number2;
+       }
+        if (largestNumber < number3){
+            largestNumber = number3;
+        }
+        System.out.println("En Büyük = " + largestNumber);
+    }
+}
