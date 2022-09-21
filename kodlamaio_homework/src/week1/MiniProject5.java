6c136f8155a3d9a6eb0b2c3d2d6efa3fc3fbaebc
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2022-09-21 20:10:52 +0300

    Mini Project 5 "Find Numbers"

---
+++ kodlamaio_homework/src/week1/MiniProject5.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class MiniProject5 {
+    public static void main(String[] args) {
+       int[] numbers = new int[]{1,2,5,7,9,0};
+       int search =5;
+       boolean isThere=false;
+
+       for (int number: numbers){
+           if (number==search){
+               isThere=true;
+               break;
+           }
+       }
+       if (isThere){
+           System.out.println("Sayı mevcuttur");
+       }else {
+           System.out.println("Sayı mevcut değildir");
+       }
+    }
+}
